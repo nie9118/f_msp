0ca01bc0ad4ab7ad660baa1301f1d52f19ba7041
shengbindashuai <2714971505@qq.com>
2026-01-23 00:05:04 +0800

    s2

--- Orion-MSP-main/scripts/train_stage2.sh
+++ Orion-MSP-main/scripts/train_stage2.sh
@@ -6,107 +6,52 @@
 # Generate prior datasets on the fly
 # ----------------------------------
 
-torchrun --standalone --nproc_per_node=1 /path/to/orion_msp/train/run.py \
+torchrun  --nproc_per_node=8 /vast/users/guangyi.chen/causal_group/zijian.li/dmir_crl/f_msp/Orion-MSP-main/src/orion_msp/train/run.py \
             --wandb_log True \
-            --wandb_project Orion-MSP \
-            --wandb_name Stage2 \
-            --wandb_dir /my/wandb/dir \
-            --wandb_mode online \
+            --wandb_project TabICL \
+            --wandb_name fang \
+            --wandb_dir ./wandb/dir \
+            --wandb_mode offline \
             --device cuda \
             --dtype float32 \
             --np_seed 42 \
             --torch_seed 42 \
-            --max_steps 2000 \
-            --batch_size 512 \
+            --max_steps 20000 \
+            --batch_size 256 \
             --micro_batch_size 1 \
-            --lr 2e-5 \
-            --scheduler polynomial_decay_warmup \
-            --warmup_proportion 0 \
-            --poly_decay_lr_end 5e-6 \
-            --poly_decay_power 2.0 \
+            --lr 0.75e-4 \
+            --scheduler cosine_warmup \
+            --warmup_proportion 0.01 \
             --gradient_clipping 1.0 \
             --prior_type mix_scm \
             --prior_device cpu \
-            --batch_size_per_gp 4 \
+            --batch_size_per_gp 1 \
             --min_features 2 \
             --max_features 100 \
             --max_classes 10 \
-            --max_seq_len 1024 \
+            --max_seq_len 4096 \
             --min_train_size 0.1 \
             --max_train_size 0.9 \
-            --embed_dim 128 \
+            --embed_dim 96 \
             --col_num_blocks 3 \
             --col_nhead 4 \
             --col_num_inds 128 \
-            --row_num_blocks 6 \
-            --row_nhead 8 \
-            --row_num_cls 4 \
+            --row_num_blocks 3 \
+            --row_nhead 4 \
+            --row_num_cls 2 \
             --row_rope_base 100000 \
-            --row_scales 1,4,16 \
-            --row_window 8 \
+            --row_scales 1,4 \
+            --row_window 4 \
             --row_num_random 2 \
-            --row_num_global 8 \
+            --row_num_global 2 \
             --row_group_mode pma \
             --perc_num_latents 64 \
             --perc_layers 3 \
-            --icl_num_blocks 12 \
+            --icl_num_blocks 6 \
             --icl_nhead 4 \
             --ff_factor 2 \
             --norm_first True \
-            --checkpoint_dir /my/stage1/checkpoint/dir \
+            --checkpoint_dir ./checkpoints_s2/rowmixer_lite_icl \
+            --checkpoint_path ./checkpoints/rowmixer_lite_icl/step-12000.ckpt \
             --save_temp_every 50 \
             --save_perm_every 1000
-
-
-
-# Loading prior data from disk and training
-torchrun --standalone --nproc_per_node=1 /path/to/orion_msp/train/run.py \
-            --wandb_log True \
-            --wandb_project Orion-MSP \
-            --wandb_name Stage2 \
-            --wandb_dir /my/wandb/dir \
-            --wandb_mode online \
-            --device cuda \
-            --dtype float32 \
-            --np_seed 42 \
-            --torch_seed 42 \
-            --max_steps 2000 \
-            --batch_size 512 \
-            --micro_batch_size 1 \
-            --lr 2e-5 \
-            --scheduler polynomial_decay_warmup \
-            --warmup_proportion 0 \
-            --poly_decay_lr_end 5e-6 \
-            --poly_decay_power 2.0 \
-            --gradient_clipping 1.0 \
-            --prior_dir /my/stage1/prior/dir \
-            --load_prior_start 0 \
-            --delete_after_load False \
-            --prior_device cpu \
-            --embed_dim 128 \
-            --col_num_blocks 3 \
-            --col_nhead 4 \
-            --col_num_inds 128 \
-            --row_num_blocks 6 \
-            --row_nhead 8 \
-            --row_rope_base 100000 \
-            --row_scales 1,4,16 \
-            --row_window 8 \
-            --row_num_random 2 \
-            --row_num_global 4 \
-            --row_num_cls 4 \
-            --row_group_mode pma \
-            --perc_num_latents 64 \
-            --perc_layers 3 \
-            --icl_num_blocks 12 \
-            --icl_nhead 4 \
-            --ff_factor 2 \
-            --norm_first True \
-            --checkpoint_dir /my/stage1/checkpoint/dir \
-            --save_temp_every 50 \
-            --save_perm_every 500
-
-
-
-
-
